fb83daf86c9462da2fe830d5aa59c5ed0083ca95
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-28 10:09:27 +0530

    cart functions successful

--- src/main/java/Shopping_Cart/model/Cart.java
+++ src/main/java/Shopping_Cart/model/Cart.java
@@ -17,17 +17,31 @@ import java.util.Set;
  * Cart is NOT persisted in the database.
  */
 @Data
+
 public class Cart {
 
     //using a Set. Sets do not allow duplicate entries. This will come in handy when we add items to the cart.
-    private Set<CartItem> cartItems;
-    private User user;
-    private double totalPrice;
+    private Set<CartItem> cartItems = new java.util.HashSet<>();
+    private int user;
+    private double totalPrice = 0;
     private String currency;
+
+    public Cart(int user, String currency) {
+        this.user = user;
+        this.currency = currency;
+    }
 
     public void addToCart(CartItem cartItem) {
         if (cartItems.contains(cartItem)) {
-            cartItems.stream().filter(item -> item.equals(cartItem)).forEach(item -> item.setQuantity(item.getQuantity() + 1));
+            try {
+                CartItem item = cartItems.stream().filter(i -> i.getId() == cartItem.getId()).findFirst().get();
+                cartItems.remove(item);
+                item.setQuantity(item.getQuantity() + 1);
+                cartItems.add(item);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         } else {
             cartItems.add(cartItem);
         }
@@ -36,9 +50,17 @@ public class Cart {
 
     public void reduceFromCart(CartItem cartItem) {
         if (cartItems.contains(cartItem)) {
-            cartItems.stream().filter(item -> item.equals(cartItem)).forEach(item -> item.setQuantity(item.getQuantity() - 1));
+            try {
+                CartItem item = cartItems.stream().filter(i -> i.getId() == cartItem.getId()).findFirst().get();
+                cartItems.remove(item);
+                item.setQuantity(item.getQuantity() - 1);
+                cartItems.add(item);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         } else {
-            cartItems.remove(cartItem);
+            cartItems.add(cartItem);
         }
     }
 
--- src/test/java/Shopping_Cart/model/CartTest.java
+++ src/test/java/Shopping_Cart/model/CartTest.java
@@ -6,10 +6,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CartTest {
 
-    private Cart cart;
+    private Cart cart = new Cart(1, "USD");
     @Test
     void addToCart() {
-        cart.addToCart(new CartItem(1, "phone", 100, "USD", 1, "imgUrl"));
+        CartItem item = new CartItem(1, "phone", 100, "USD", 1, "imgUrl");
+        CartItem item2 = new CartItem(2, "apple", 100, "USD", 1, "imgUrl");
+        cart.addToCart(item);
+        cart.addToCart(item);
+        cart.addToCart(item);
+        cart.addToCart(item);
+
+        cart.addToCart(item2);
+        cart.addToCart(item2);
+        cart.reduceFromCart(item2);
+
+        System.out.println(cart.getCartItems());
     }
 
     @Test
@@ -22,5 +33,6 @@ class CartTest {
 
     @Test
     void getCartItems() {
+        System.out.println(cart.getCartItems());
     }
 }
